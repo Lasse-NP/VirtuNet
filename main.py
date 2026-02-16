7e3276272aef254c4a8594ae8083b601429cceff
Magnus Rasmussen <138506946+magn9195@users.noreply.github.com>
2026-02-16 11:29:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from nicegui import ui
 ui.dark_mode().enable()
 
 ui.label('This is in dark mode!')
-ui.button('A dark button')
+ui.button('A dark button', on_click=lambda: ui.label('Lasse er en fucking adam'))
 
 ui.run(native=True)
