1f4d902ecf7837587c4c91cdaad1a04f1e479175
Lasse-NP <10632094@ucn.dk>
2026-03-01 21:20:47 +0100

    Setup changes to get ready for coding.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from GUI import frontpage
+import logging
 
 def fetchdependencies():
+    logging.info('No Dependencies Fetched')
     # System Dependencies to fetch: MiniNet, OpenvSwitch, OpenVPN
 
 if __name__ == '__main__':
