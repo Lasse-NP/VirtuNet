2a414e173dd8b033a1670dcaadec369463bd01cf
Lasse-NP <10632094@ucn.dk>
2026-03-02 10:13:25 +0100

    Dependency detection function

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from GUI import frontpage
-import logging
+import shutil
+import sys
 
-def fetchdependencies():
-    logging.info('No Dependencies Fetched')
-    # System Dependencies to fetch: MiniNet, OpenvSwitch, OpenVPN
+REQUIRED_COMMANDS = [
+    'openvpn',      #OpenVPN
+    'ovs-vsctl',    #OVSwitch
+    'easy-rsa',     #EasyRSA
+    'mn'            #MiniNet
+]
+
+def check_dependencies():
+    missing = []
+    for cmd in REQUIRED_COMMANDS:
+        if shutil.which(cmd) is None:
+            missing.append(cmd)
+
+    if missing:
+        print('Error! The following required dependencies are missing:')
+        for dep in missing:
+            print(f' - {dep}')
+        print('\nPlease install them before running this application.')
+        sys.exit(1)
+
+    print('All dependencies satisfied.')
 
 if __name__ == '__main__':
-    fetchdependencies()
+    check_dependencies()
     frontpage.startgui()
