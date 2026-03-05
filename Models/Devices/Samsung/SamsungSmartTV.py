b3a867083ea5bafbe86b37bc07cb762b3a4267ab
Abdiweli Qaasim Ali <qabdiweli@gmail.com>
2026-03-05 12:22:30 +0100

    New Sansung device

---
+++ Models/Devices/Samsung/SamsungSmartTV.py
@@ -0,0 +1,11 @@
+from faker import faker
+
+from Models.Vendor.Samsung import Samsung
+
+
+class SamsungSmartTV(Samsung):
+
+ def __init__(self):
+     fake = Faker()
+     device_name = f'{fake.first.name()}-SamsungSmartTV-{fake.uuid4()[:4]}'
+     super().__init__(device_name)
