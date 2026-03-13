539cee302806ea1ecac13b455c5908f41b76cd87
Lasse-NP <10632094@ucn.dk>
2026-03-13 09:56:49 +0100

    Fixed wrong formatting on class

--- Models/Devices/Samsung/SamsungSmartTV.py
+++ Models/Devices/Samsung/SamsungSmartTV.py
@@ -4,9 +4,8 @@ from Models.Vendor.Samsung import Samsung
 
 
 class SamsungSmartTV(Samsung):
-
- def __init__(self):
-     fake = Faker()
-     device_name = f'{fake.first.name()[:4]}-SamTV'
-     device_os = 'Android'
-     super().__init__(device_name[:10], device_os)
+     def __init__(self):
+         fake = Faker()
+         device_name = f'{fake.first.name()[:4]}-SamTV'
+         device_os = 'Android'
+         super().__init__(device_name[:10], device_os)
