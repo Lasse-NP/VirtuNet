6397970084031e650c7331c385d99ee2095862e8
Abdiweli Qaasim Ali <qabdiweli@gmail.com>
2026-04-13 12:02:30 +0200

    New Elektronic devices

--- Models/Devices/Sony/SonySmartTV.py
+++ Models/Devices/Sony/SonySmartTV.py
@@ -3,4 +3,13 @@ from faker import Faker
 
 from Models.Fingerprints.OS.Linux import AndroidTV
 from Models.Fingerprints.Services import HTTP, HTTPS
-from Models.Fingerprints.
+from Models.Devices.Vendor.Sony import Sony
+
+class SonySmartTV(Sony):
+    def __init__(self):
+        fake = Faker()
+        super().__init__(
+            f"{fake.first.name()[:4]}-SonTV"[:10],
+            AndroidTV,
+            services=[HTTP(), HTTPS()]
+        )
