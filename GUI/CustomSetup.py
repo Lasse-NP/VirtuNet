36c05f2d9e238d13a3c6f61a9619ca2fcf27d2c8
LasseNP <10632094@ucn.dk>
2026-04-29 10:51:26 +0200

    Small bug fix to prevent hosts being nameless (Hosts break if you don't have at least 1 character)

--- GUI/CustomSetup.py
+++ GUI/CustomSetup.py
@@ -131,7 +131,7 @@ def custom_setup_page():
                 ui.notify("Name cannot be above 10 characters.", type='negative')
                 return
 
-            if len(input_name.strip()) > 0:
+            if len(input_name.strip()) == 0:
                 ui.notify("Name must consist of at least 1 character.", type='negative')
                 return
 
